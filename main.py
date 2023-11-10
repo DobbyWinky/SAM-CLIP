d692d1de6fbe75880b5cb3e13644e18ccd3b90af
DobbyWinky <madhangi1998@gmail.com>
2023-11-10 17:03:12 -0600

    Main SAM-CLIP merge

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pickle
 import numpy as np
 import torch
 from segment_anything import sam_model_registry, SamPredictor
+from torch.nn import functional as F
+import cv2
 
 class SAM_Feat:
     def __init__(self, sam_model,
@@ -11,9 +13,10 @@ class SAM_Feat:
         self.sam = sam_model_registry[sam_model_type](checkpoint=sam_model)
         self.sam.to(self.device)
         self.predict = SamPredictor(self.sam)
+        self.currImage = None
 
     def read_prev_data(self, data_dir, frameID, objID):
-        pObj = os.path.join(data_dir, 'objects', f'{objID:05d}', f'frame_{frameID:05d}_prev_data.pkl')
+        pObj = os.path.join(data_dir, 'objects', f'{objID:05d}', f'frame_{frameID:05d}.pkl')
         if not os.path.exists(pObj):
             return None, [], []
         with open(pObj, "rb") as fObj:
@@ -24,9 +27,12 @@ class SAM_Feat:
         mask = (mask * 255).astype(np.uint8)
         rgb_mask = np.repeat(mask, 3, axis=-1)
 
+        self.predict.set_image(self.currImage)
+
         input_mask = self.predict.transform.apply_image(rgb_mask)
+
         input_mask_torch = torch.as_tensor(input_mask, device=self.device)
-        input_mask_torch = input_mask_torch.permute(2, 0, 1).contiguous()[None, :, :, :]
+        input_mask_torch = input_mask_torch.permute(2,0,1).contiguous()[None, :, :, :]
         mask = self.predict.model.preprocess(input_mask_torch)
         feats = self.predict.features.squeeze().permute(1, 2, 0)
 
@@ -40,23 +46,31 @@ class SAM_Feat:
         return feats
     
     def get_features_from_array(self, masks):
-        print("inside get_features_from_array")
         rgb_mask = (masks.astype(np.uint8) * 255).repeat(3, axis=-1)
-        feats = [self.get_feat(mask) for mask in rgb_mask]
+        feats = self.get_features(masks[::])
+        print("Hello", feats.shape)
         return feats
 
-# Step0: Initialize SAM model
-model_dir = "models/segment_anything"
-sam_checkpoint = model_dir+"/sam_vit_b_01ec64.pth"
-model_type = "vit_b"
-sam = SAM_Feat(sam_checkpoint, model_type)
-#Change this to the path where your frames are present
-# Step1: Read the masks from .pkl files
-masks = sam.read_prev_data("/Users/madhangi/Downloads/test", 1, 1)
 
-# Step2: Read image file
+def get_SAM_features():
+    # Step0: Initialize SAM model
+    model_dir = "models/segment_anything"
+    sam_checkpoint = model_dir+"/sam_vit_b_01ec64.pth"
+    model_type = "vit_b"
+    sam = SAM_Feat(sam_checkpoint, model_type)
+    #Change this to the path where your frames are present
+    # Step1: Read the masks from .pkl files
+    masks = sam.read_prev_data("/Users/madhangi/Downloads/test", 1, 1)
 
+    # Step2: Read image file
+    sam.currImage = cv2.imread("/Users/madhangi/Downloads/test/frames/frame_00001.jpg")
 
-# Step3: Get features sam
-sam_feat = sam.get_features_from_array(masks)
+    binary_masks = masks[0]
+    # Step3: Get features sam
+    sam_feat = sam.get_features_from_array(binary_masks)
+
+    return sam_feat
+
+def get_CLIP_features():
+    # TODO
 
